0072c17455032bf83dda2f43892b69a3beb509f5
Terentev Nikita <nitro-2003@mail.ru>
2023-05-13 21:33:30 +0700

    Started writing code.

--- app/src/main/java/com/example/pomodoroapp/MainActivity.java
+++ app/src/main/java/com/example/pomodoroapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.pomodoroapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.pomodoroapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    // ViewBinding. activity_main ==> ActivityMainBinding
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.sideBarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TimerConfiguration.class);
+                startActivity(intent);
+            }
+        });
     }
 }
