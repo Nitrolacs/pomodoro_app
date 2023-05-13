e55e8b79a1980d1295662ade022f4a3f1e398f2a
Terentev Nikita <nitro-2003@mail.ru>
2023-05-14 00:08:02 +0700

    Semi-working condition.

--- app/src/main/java/com/example/pomodoroapp/MainActivity.java
+++ app/src/main/java/com/example/pomodoroapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.pomodoroapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 
 import com.example.pomodoroapp.databinding.ActivityMainBinding;
@@ -12,6 +14,19 @@ public class MainActivity extends AppCompatActivity {
 
     // ViewBinding. activity_main ==> ActivityMainBinding
     private ActivityMainBinding binding;
+
+    private String nameConfiguration;
+    private Integer focusMinutes, restMinutes, roundsCount;
+
+    private CountDownTimer focusTimer, restTimer, startTimer;
+
+    private int mRound = 1;
+
+    private boolean isFocusing = true;
+    private boolean isRest = false;
+
+    private MediaPlayer mp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +37,343 @@ public class MainActivity extends AppCompatActivity {
         binding.sideBarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, TimerConfiguration.class);
+                Intent intent = new Intent(MainActivity.this, TimerConfigurationActivity.class);
                 startActivity(intent);
             }
         });
+
+        // Receive Extras
+        nameConfiguration = getIntent().getStringExtra("name");
+        focusMinutes = getIntent().getIntExtra("focus", 0) * 60 * 1000;
+        restMinutes = getIntent().getIntExtra("rest", 0) * 60 * 1000;
+        roundsCount = getIntent().getIntExtra("roudns", 0);
+        // Set Rounds Text
+        binding.studyStageNumber.setText(mRound + "/" + roundsCount);
+        // Start Timer
+        setStartTimer();
+        // Reset Button
+        binding.buttonStop.setOnClickListener(v -> resetOrStart());
+    }
+
+    // Set Rest Timer
+    private void setStartTimer() {
+        // playSound();
+
+        binding.studyStageText.setText("Приготовьтесь");
+        binding.progressBar.setProgress(0);
+        binding.progressBar.setMax(10);
+
+        startTimer = new CountDownTimer(10500, 1000) {
+            @Override
+            public void onTick(long p0) {
+                binding.progressBar.setProgress((int)(p0 / 1000));
+                binding.timer.setText(String.valueOf(p0 / 1000));
+            }
+
+            @Override
+            public void onFinish() {
+                mp.reset();
+                if (isFocusing) {
+                    setupFocusingView();
+                } else {
+                    setupRestView();
+                }
+            }
+        }.start();
+    }
+
+    private void setupRestView() {
+        binding.studyStageNumber.setText("Break Time");
+        binding.progressBar.setMax(restMinutes/1000);
+
+        if (restTimer != null)
+            restTimer.cancel();
+
+        setRestTimer();
+    }
+
+    // Set Rest Timer
+    private void setRestTimer() {
+        restTimer = new CountDownTimer(restMinutes.longValue() + 500,
+                1000 ) {
+            @Override
+            public void onTick(long p0) {
+                binding.progressBar.setProgress((int)(p0 / 1000));
+                binding.timer.setText(createTimeLabels((int)(p0 / 1000)));
+            }
+
+            @Override
+            public void onFinish() {
+                isFocusing = true;
+                setStartTimer();
+            }
+
+        }.start();
+    }
+
+    // Prepare Screen for Focusing Timer
+    private void setupFocusingView() {
+        binding.studyStageNumber.setText(mRound + "/" + roundsCount);
+        binding.studyStageText.setText("Study Time");
+        binding.progressBar.setMax(focusMinutes/1000);
+
+        if (focusTimer != null)
+            focusTimer.cancel();
+
+        setFocusingTimer();
+    }
+
+    // Set Focusing Timer
+    private void setFocusingTimer() {
+        focusTimer = new CountDownTimer(focusMinutes.longValue() + 500, 1000) {
+            @Override
+            public void onTick(long p0) {
+                binding.progressBar.setProgress((int) p0/ 1000);
+                binding.timer.setText(createTimeLabels((int) (p0 / 1000)));
+            }
+
+            @Override
+            public void onFinish() {
+                if (mRound < roundsCount) {
+                    isFocusing = false;
+                    setStartTimer();
+                    mRound++;
+                } else {
+                    clearAttribute();
+                    binding.studyStageText.setText("Вы закончили все циклы :)");
+                }
+            }
+        }.start();
+    }
+
+
+
+    // Rest Whole Attributes in MainActivity
+    private void clearAttribute() {
+        binding.studyStageText.setText("Нажми кнопку старта чтобы перезапустить");
+        binding.buttonStop.setImageResource(R.drawable.ic_play);
+        binding.progressBar.setProgress(0);
+        binding.timer.setText("0");
+        mRound = 1;
+        binding.studyStageNumber.setText(mRound + "/" + roundsCount);
+        restTimer.cancel();
+        focusTimer.cancel();
+        startTimer.cancel();
+        mp.reset();
+        isRest = true;
+    }
+
+    // Convert Received Numbers to Minutes and Seconds
+    private String createTimeLabels(int time) {
+        String timeLabel = "";
+        int minutes = time / 60;
+        int seconds = time % 60;
+
+        if (minutes < 10) timeLabel += "0";
+        timeLabel += minutes + ":";
+
+        if (seconds < 10) timeLabel += "0";
+        timeLabel += seconds;
+
+        return timeLabel;
+    }
+
+    // For Reset or Restart Pomodoro
+    private void resetOrStart() {
+        if (isRest) {
+            binding.buttonStop.setImageResource(R.drawable.ic_stop);
+            setRestTimer();
+            isRest = false;
+        } else {
+            clearAttribute();
+        }
     }
 }
+
+/*
+import android.media.MediaPlayer;
+import android.net.Uri;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import com.exo.pomodoro.databinding.ActivityFeedBinding;
+
+public class FeedActivity extends AppCompatActivity {
+    private ActivityFeedBinding binding;
+
+    private Integer studyMinute = null;
+    private Integer breakMinute = null;
+    private Integer roundCount = null;
+
+    private CountDownTimer restTimer = null;
+    private CountDownTimer studyTimer = null;
+    private CountDownTimer breakTimer = null;
+
+    private int mRound = 1;
+
+    private boolean isStudy = true;
+
+    private boolean isStop = false;
+
+    private MediaPlayer mp = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityFeedBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        // Receive Extras
+        studyMinute = getIntent().getIntExtra("study", 0) * 60 * 1000;
+        breakMinute = getIntent().getIntExtra("break", 0) * 60 * 1000;
+        roundCount = getIntent().getIntExtra("round", 0);
+        // Set Rounds Text
+        binding.tvRound.setText(mRound + "/" + roundCount);
+        //Start Timer
+        setRestTimer();
+        // Reset Button
+        binding.ivStop.setOnClickListener(v -> resetOrStart());
+    }
+    // Set Rest Timer
+    private void setRestTimer(){
+       playSound();
+        binding.tvStatus.setText("Get Ready");
+        binding.progressBar.setProgress(0);
+        binding.progressBar.setMax(10);
+        restTimer = new CountDownTimer(10500,1000) {
+            @Override
+            public void onTick(long p0) {
+                binding.progressBar.setProgress((int)(p0 / 1000));
+                binding.tvTimer.setText(String.valueOf(p0 / 1000));
+            }
+            @Override
+            public void onFinish() {
+                mp.reset();
+                if (isStudy){
+                    setupStudyView();
+                }else{
+                    setupBreakView();
+                }
+            }
+        }.start();
+    }
+    // Set Study Timer
+    private void setStudyTimer(){
+
+        studyTimer = new CountDownTimer(studyMinute.longValue() + 500,1000) {
+            @Override
+            public void onTick(long p0) {
+                binding.progressBar.setProgress((int)(p0 /1000));
+                binding.tvTimer.setText(createTimeLabels((int)(p0 / 1000)));
+            }
+            @Override
+            public void onFinish() {
+                if(mRound < roundCount){
+                    isStudy = false;
+                    setRestTimer();
+                    mRound++;
+                }else{
+                    clearAttribute();
+                    binding.tvStatus.setText("You have finish your rounds :)");
+                }
+            }
+        }.start();
+    }
+    // Set Break Timer
+    private void setBreakTimer() {
+        breakTimer = new CountDownTimer(breakMinute.longValue()+500, 1000 ) {
+            @Override
+            public void onTick(long p0) {
+                binding.progressBar.setProgress((int)(p0 / 1000));
+                binding.tvTimer.setText(createTimeLabels((int)(p0 / 1000)));
+            }
+
+            @Override
+            public void onFinish() {
+                isStudy = true;
+                setRestTimer();
+            }
+
+        }.start();
+    }
+    // Prepare Screen for Study Timer
+    private void setupStudyView() {
+        binding.tvRound.setText(mRound + "/" + roundCount);
+        binding.tvStatus.setText("Study Time");
+        binding.progressBar.setMax(studyMinute/1000);
+
+        if (studyTimer != null)
+            studyTimer.cancel();
+
+        setStudyTimer();
+    }
+    // Prepare Screen for Study Timer
+    private void setupBreakView() {
+        binding.tvStatus.setText("Break Time");
+        binding.progressBar.setMax(breakMinute/1000);
+
+        if (breakTimer != null)
+            breakTimer.cancel();
+
+        setBreakTimer();
+    }
+    // Initialize sound file to MediaPlayer
+    private void playSound() {
+
+        try {
+            Uri soundUrl = Uri.parse("android.resource://com.exo.pomodoro/" + R.raw.count_down);
+            mp = MediaPlayer.create(this,soundUrl);
+            mp.setLooping(false);
+            mp.start();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    // Rest Whole Attributes in FeedActivity
+    private void clearAttribute() {
+        binding.tvStatus.setText("Press Play Button to Restart");
+        binding.ivStop.setImageResource(R.drawable.ic_play);
+        binding.progressBar.setProgress(0);
+        binding.tvTimer.setText("0");
+        mRound = 1;
+        binding.tvRound.setText(mRound + "/" + roundCount);
+        restTimer.cancel();
+        studyTimer.cancel();
+        breakTimer.cancel();
+        mp.reset();
+        isStop = true;
+    }
+    // Convert Received Numbers to Minutes and Seconds
+    private String createTimeLabels(int time) {
+        String timeLabel = "";
+        int minutes = time / 60;
+        int secends = time % 60;
+
+        if (minutes < 10) timeLabel += "0";
+        timeLabel += minutes + ":";
+
+        if (secends < 10) timeLabel += "0";
+        timeLabel += secends;
+
+        return timeLabel;
+    }
+    // For Reset or Restart Pomodoro
+    private void resetOrStart() {
+        if (isStop) {
+            binding.ivStop.setImageResource(R.drawable.ic_stop);
+            setRestTimer();
+            isStop = false;
+        } else {
+            clearAttribute();
+        }
+    }
+    // Clear Everything When App Destroyed
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        restTimer.cancel();
+        studyTimer.cancel();
+        breakTimer.cancel();
+        mp.reset();
+    }
+ */
---
+++ app/src/main/java/com/example/pomodoroapp/TimerConfigurationActivity.java
@@ -0,0 +1,54 @@
+package com.example.pomodoroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.pomodoroapp.databinding.ActivityTimerConfigurationBinding;
+
+public class TimerConfigurationActivity extends AppCompatActivity {
+
+    // ViewBinding. activity_main ==> ActivityMainBinding
+    private ActivityTimerConfigurationBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityTimerConfigurationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.sideBarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TimerConfigurationActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        binding.buttonSaveConfiguration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String configurationName = binding.fieldConfigurationName.getText().toString();
+                String focusingTime = binding.fieldFocusingTime.getText().toString();
+                String restTime = binding.fieldRestTime.getText().toString();
+                String roundsNumber = binding.fieldRoundsNumber.getText().toString();
+
+                if (!configurationName.isEmpty() && !focusingTime.isEmpty() && !restTime.isEmpty()
+                        && !roundsNumber.isEmpty()) {
+                    Intent intent = new Intent(TimerConfigurationActivity.this, MainActivity.class);
+                    intent.putExtra("name", configurationName);
+                    intent.putExtra("focus", Integer.parseInt(focusingTime));
+                    intent.putExtra("rest", Integer.parseInt(restTime));
+                    intent.putExtra("rounds", Integer.parseInt(roundsNumber));
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(TimerConfigurationActivity.this, "Заполните все поля",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
