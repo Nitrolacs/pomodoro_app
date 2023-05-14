e7822a19c7cfa1616d92263c589672933abacaf2
Terentev Nikita <nitro-2003@mail.ru>
2023-05-15 00:09:52 +0700

    Did TimerConfigurationActivity refactoring.

--- app/src/main/java/com/example/pomodoroapp/TimerConfigurationActivity.java
+++ app/src/main/java/com/example/pomodoroapp/TimerConfigurationActivity.java
@@ -14,6 +14,49 @@ public class TimerConfigurationActivity extends AppCompatActivity {
 
     private ActivityTimerConfigurationBinding binding;
 
+    private void startMainActivity(String configurationName, String focusingTime, String restTime,
+                                   String roundsNumber) {
+        Intent intent = new Intent(TimerConfigurationActivity.this, MainActivity.class);
+        intent.putExtra("name", configurationName.trim());
+        intent.putExtra("focus", Integer.parseInt(focusingTime));
+        intent.putExtra("rest", Integer.parseInt(restTime));
+        intent.putExtra("rounds", Integer.parseInt(roundsNumber));
+        startActivity(intent);
+    }
+
+    private void checkInput(String configurationName, String focusingTime, String restTime,
+                       String roundsNumber) {
+        // Получаем массив непрошедших проверку параметров
+        String[] invalidParameters = InputChecker.getInputChecker().checkData(configurationName,
+                focusingTime, restTime, roundsNumber);
+
+        // Проверяем, что массив не пустой
+        if (invalidParameters.length > 0) {
+
+            // Проходим по всем элементам массива с помощью цикла for-each
+            for (String parameter : invalidParameters) {
+
+                // Сравниваем название параметра с названием поля
+                switch (parameter) {
+                    case "configurationName":
+                        binding.fieldConfigurationName.setError("Введите непустое значение");
+                        break;
+                    case "focusingTime":
+                        binding.fieldFocusingTime.setError("Введите число от 1 до 60");
+                        break;
+                    case "restTime":
+                        binding.fieldRestTime.setError("Введите число от 1 до 60");
+                        break;
+                    case "roundsNumber":
+                        binding.fieldRoundsNumber.setError("Введите число от 1 до 20");
+                        break;
+                }
+            }
+        } else {
+            startMainActivity(configurationName, focusingTime, restTime, roundsNumber);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,41 +69,7 @@ public class TimerConfigurationActivity extends AppCompatActivity {
             String restTime = Objects.requireNonNull(binding.fieldRestTime.getText()).toString();
             String roundsNumber = Objects.requireNonNull(binding.fieldRoundsNumber.getText()).toString();
 
-            // Получаем массив непрошедших проверку параметров
-            String[] invalidParameters = InputChecker.getInputChecker().checkData(configurationName,
-                    focusingTime, restTime, roundsNumber);
-
-            // Проверяем, что массив не пустой
-            if (invalidParameters.length > 0) {
-
-                // Проходим по всем элементам массива с помощью цикла for-each
-                for (String parameter : invalidParameters) {
-
-                    // Сравниваем название параметра с названием поля
-                    switch (parameter) {
-                        case "configurationName":
-                            binding.fieldConfigurationName.setError("Введите непустое значение");
-                            break;
-                        case "focusingTime":
-                            binding.fieldFocusingTime.setError("Введите число от 1 до 60");
-                            break;
-                        case "restTime":
-                            binding.fieldRestTime.setError("Введите число от 1 до 60");
-                            break;
-                        case "roundsNumber":
-                            binding.fieldRoundsNumber.setError("Введите число от 1 до 20");
-                            break;
-                    }
-                }
-
-            } else {
-                Intent intent = new Intent(TimerConfigurationActivity.this, MainActivity.class);
-                intent.putExtra("name", configurationName.trim());
-                intent.putExtra("focus", Integer.parseInt(focusingTime));
-                intent.putExtra("rest", Integer.parseInt(restTime));
-                intent.putExtra("rounds", Integer.parseInt(roundsNumber));
-                startActivity(intent);
-            }
+            checkInput(configurationName, focusingTime, restTime, roundsNumber);
         });
     }
 }
